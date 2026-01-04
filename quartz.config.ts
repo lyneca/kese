d8ae2264b42e701a39c9c97ecb021cb567bc872e
lyneca <lukemtuthill@gmail.com>
2026-01-05 02:48:47 +1100

    Fix missing quote

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Lands of Kese,
+    pageTitle: "The Lands of Kese",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
